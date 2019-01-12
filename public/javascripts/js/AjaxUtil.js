66cc329aee228184ec0462923bfae8457bd892d1
srajathii <srajathii@paypal.com>
2019-01-12 19:13:59 +0530

    Ajax Post from CLient Simplified

--- public/javascripts/js/AjaxUtil.js
+++ public/javascripts/js/AjaxUtil.js
@@ -10,11 +10,10 @@ var AjaxUtils = {
         var message = $("#user-message").val();
         alert(name+"-"+email+"-"+mobile+"-"+course+"-"+location+"-"+message);
 
-        if (name !== null && name !== '' && email !== null && email !== '' && mobile !== null && mobile !== '' && course !== null && course !== '' && location !== null && location !== ''&& message !== null && message !== '') {
-            $.ajax({
-                type: "post",
-                url: "http://localhost:3000/emailService",
-                data: "name=" + name + "&email=" + email + "&mobile=" + mobile + "&course=" + course + "&location=" + location + "&message=" + message,
+        if (name !== null && name !== '' && email !== null && email !== '' && mobile !== null && mobile !== '' && course !== null && course !== '' && location !== null && location !== ''&& message !== null && message !== '') 
+        {
+            user_details= "name=" + name + "&email=" + email + "&mobile=" + mobile + "&course=" + course + "&location=" + location + "&message=" + message,
+            $.post('/emailService',user_details, function(text) {
                 // success: function(text) {
                 //     if (text == "success") {
                 //         $("#contactForm")[0].reset();
