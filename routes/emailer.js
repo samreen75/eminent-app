031629f7704158c7407cab843ee43cf37fcd7525
code-eminent <46237646+code-eminent@users.noreply.github.com>
2019-01-13 13:03:25 +0530

    added emailer file

---
+++ routes/emailer.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+    sendMail(name, fromEmail, mobile,course,location, message) {
+
+        let response = 'success';
+
+        var nodemailer = require('nodemailer');
+
+        // Create the transporter with the required configuration for Gmail
+        // change the user and pass !
+        var transporter = nodemailer.createTransport({
+            host: 'smtp.zoho.com',
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: 'dontreply@eminentinnovative.com',
+                pass: 'providepasswordhere' 
+            }
+
+        });
+
+        // setup e-mail data, even with unicode symbols
+        var mailOptions = {
+            from: '"Enquiry " dontreply@eminentinnovative.com', // sender address (who sends)
+            to: 'info@eminentinnovative.com', // list of receivers (who receives)
+            //bcc: 'info@eminentinnovative.com',
+            subject: 'EminentQuery',
+            html: " Name - " + name + "<br>Sender :- " + fromEmail + "<br> Phone:- " + mobile + "<br> Course:- " + course + "<br> Location:- " + location + "<br> Message:- " + message //'<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function(error, info) {
+            console.log(mailOptions);
+
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                return response;
+
+            }
+
+        });
+    }
+};
